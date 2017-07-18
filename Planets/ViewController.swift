1b0dd774f7d63c15049d0421483c500b8b27d3d5
COBE Osijek <daniel.pavlekovic@icloud.com>
2017-07-18 11:56:21 +0200

    Cells not showing up.

--- Planets/ViewController.swift
+++ Planets/ViewController.swift
@@ -8,10 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    var planets = [Planet]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("planet") {
+                // let range = item.startIndex..<item.index(item.startIndex, offsetBy: 7)
+                planets.append(Planet(name: item.replacingOccurrences(of: "planet_", with: ""), image: item))
+            }
+        }
+        print(planets)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +35,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return planets.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "planetCell", for: indexPath) as? PlanetTableViewCell else{
+            fatalError("Cell not an instance of PlanetTableViewCell")
+        }
+        cell.textLabel?.text = planets[indexPath.row].name
+        cell.imageView?.image = UIImage(named: planets[indexPath.row].image)
+        return cell
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
 }
-
